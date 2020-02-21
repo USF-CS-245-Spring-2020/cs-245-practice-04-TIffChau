cf92a7ddb27ba0fd1db378c3de16bd1835587006
Tiff Zhou <47542629+TIffChau@users.noreply.github.com>
2020-02-20 20:09:04 -0800

    Add files via upload

--- QuickSort.java
+++ QuickSort.java
@@ -1,49 +1,48 @@
-public class QuickSort implements SortingAlgorithm
-{
-	public void sort(int [] a)
+public class QuickSort implements SortingAlgorithm {
+	public void sort(int[] a) 
 	{
 		qs(a,0,a.length-1);
 	}
-	public void qs(int [] a, int start, int end)
+	
+	public void qs(int[] a, int start, int end) 
 	{
-		if(start<end)
+		if(start > end)
 		{
 			return;
 		}
-		//int pivot = partition; 
-		int p = partition(a,start,end);
-		qs(a,start,p-1);
-		qs(a,p+1,end);
-	}
-
-	public int partition(int [] a, int start, int end)
-	{
-		int pivot = start;
-		int left = start+1;
-		int right = end;
-		while(left < right)
+		if(start < end) 
 		{
-			while(left<end && a[left]<a[pivot])
-			{
-				++left;
+			int p = partition(a, start, end);
+			qs(a, start, p-1);
+			qs(a, p+1, end);
+		}
+	}
+	
+	public int partition(int[] a, int start, int end) {
+		if(start < end) {
+			int pivot = start;
+			int left = start + 1;
+			int right = end;
+			while(left < right) {
+				while(left < end && a[left] <= a[pivot])
+					++left;
+				while(right >= left && a[right] > a[pivot])
+					--right;
+				if(left < right)
+					swap(a, left, right);				
 			}
-			while(right>=left && a[right]>a[pivot])
-			{
-				--right;
-			}
-			if(left<right)
-			{
-				swap(a,left,right);
-			}
-			swap(a,right,pivot);
+			if(a[right] < a[pivot])
+				swap(a, right, pivot);
 			return right;
 		}
-		return left;
+		return start;
 	}
-	public void swap(int [] a, int i, int j)
-	{
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
+	
+	public void swap(int[] a, int left, int right) {
+		int temp=a[left];
+		a[left]=a[right];
+		a[right]=temp;
+		
 	}
+
 }
